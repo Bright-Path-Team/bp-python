b46a3d624cbbaf7e41f1137debc28c388e69ad7e
Leonardo <leonardorscarpitta@gmail.com>
2024-11-15 19:46:51 -0300

    feat: add search methods
    - Create new options so users can see solar panels information &
      energy efficiency.

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-"""
-Requisitos do Sistema: 
-
-1. Implementar um menu de opções com as principais funcionalidades oferecidas pelo sistema. 
-2. Realizar validações nas entradas de dados do usuário. 
-3. Aplicar adequadamente o tratamento de exceções. 
-4. Utilizar estruturas de decisão e repetição. 
-5. Utilizar funções com passagem de parâmetros e retorno. 
-6. Utilizar listas e dicionários para armazenar dados. 
-7. Utilizar arquivos JSON como para persistência de dados. 
-"""
 # Bibliotecas importadas
 from utils.methods import *
 from dotenv import load_dotenv
@@ -33,7 +22,6 @@ print(spaces)
 
 # Variáveis de ambiente
 ip_address : str = os.getenv("ip")
-masked_ip_address : str = mask_ip(ip_address)
 
 while True:
     # Imprime a logo da empresa e um divisor, facilitando assim a visualização dos dados
@@ -43,7 +31,9 @@ while True:
     print(divider)
     print("[1] - Monitoramento da placa leste")
     print("[2] - Monitoramento da placa oeste")
-    print("[3] - Gerar gráfico de eficiência")
+    print("[3] - Monitoramento da eficiência energética")
+    print("[4] - Gerar gráfico de monitoramento")
+    print("[4] - Salvar os dados em um arquivo JSON")
     print("[99] - Encerrar o programa")
     # Verifica se o número fornecido pelo usuário é do tipo inteiro
     try:
@@ -56,7 +46,13 @@ while True:
     match opcao_usuario:
         case 1:
             print(spaces)
-            pass
+            check_info(ip_address, "east")
+        case 2:
+            print(spaces)
+            check_info(ip_address, "west")
+        case 3:
+            print(spaces)
+            check_info(ip_address, "efficiency")
         case 99:
             print(spaces)
             print(logo)
